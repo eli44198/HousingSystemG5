cf756d50e3e9d213e146b721b8b3b7e9c130f78e
eli44198 <106581406+eli44198@users.noreply.github.com>
2022-07-11 23:04:50 +0000

    Update HousingSystemG5.py

--- HousingSystemG5.py
+++ HousingSystemG5.py
@@ -1,64 +1,142 @@
-import database
 from database import *
 
 print("Welcome to UG house managing system " + "\n", "please select an option")
 
-
 def home():
+
     option = int(input(
+
         "\n"
+
         "Press 1: View all house \n"
+
         "press 2: Add your house \n"
+
         "Press 3: Modify house details \n"
+
         "\n********************************************************************\n"
+
     ))
 
     #For Option 1: view all houses
+
     if option == 1:
+
         def ViewAll():
+
             print(*view_all)
+
         ViewAll()
+
         home()
 
     #For option 2: add a house function
+
     elif option == 2:
 
         def addHouse():
+
             print("Use underscore _ inplace of space")
+
             HouseName = input(str("Enter a the name of the house: \n"))
+
             HouseId = input("Enter the house Id: \n")
+
             HouseType = input("Enter the type of house: \n")
 
             db = open("database.py", "a")
 
             if HouseName not in database.view_all:
+
                 add = "\n%s = {'House_Id': \'%s\',  'Type': \'%s\', 'Name': \'%s\'} \n\n" \
+
              % (HouseName, HouseId, HouseType, HouseName)
+
                 db.write(add)
+
                 db.write("view_all.append('%s')" % HouseName)
+
                 db.close()
+
             else:
+
                 print("Please try again")
+
                 home()
+
         addHouse()
 
     #For option 3: Modify house details
-    elif option == 3:
-        def modify():
-            print(*view_all)
-            try:
-                House = input("\nWhich house do you wish to modify? \n")
-                attributes = input("\nWhat do you wish to modify? \n")
-                features = ["House_Id", "Name", "Type"]
-                print(*features)
-                
-            except:
-                print("Failed. Please try again")
-        modify()
-    else:
-        print("Please try again")
-home()
 
+    elif option == 3:
+
+        def modify(house_name):
+
+            print(*view_all)
+
+            
+
+            try:
+
+                House = input("\nWhich house do you wish to modify? \n")
+
+                attributes = input("\nWhat do you wish to modify? \n")
+
+                features = ["House_Id", "Name", "Type"]
+
+                print(*features)
+
+                details = (eval(f"database.{house}")
+
+                
+
+                
+
+                if attributes == "Name":
+
+                    print(f"\nPrevious name was >>>>>> details[name]")
+
+                    new_name = input("\nEnter a new name >>>>>>")
+
+                    database.write("\n house[Name] = new_name")
+
+                 
+
+                elif attributes == "Type":
+
+                    print(f"\nPrevious name was >>>>>> details[name]")
+
+                    new_type = input("\nEnter a new name >>>>>>")
+
+                    database.write("\n house[Type] = new_Type")
+
+                
+
+                 
+
+                elif attributes == "house_Id":
+
+                    print(f"\nPrevious name was >>>>>> details[House_Id]")
+
+                    new_name = input("\nEnter a new name >>>>>>")
+
+                    database.write("\n house[House_Id] = new_id")
+
+                
+
+                
+
+            except:
+
+                print("Failed. Please try again")
+
+        modify()
+
+    else:
+
+        print("Please try again")
+
+home()
 
 
 
