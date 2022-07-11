c2b75344036860492ba67983b06b2ba98a3c1574
eli44198 <106581406+eli44198@users.noreply.github.com>
2022-07-11 22:32:14 +0000

    Update and rename database.txt to database.py

---
+++ database.py
@@ -0,0 +1,15 @@
+
+view_all = ["The_Loft", "Villa_Peaceful", "Adoley_Resident", "Paddocks", "Mensah_Lodge"]
+
+The_Loft = {"house_id":"V1122","Type":"Condo", "Name":"The_Loft"}
+
+Villa_Peaceful = {"House_id":"VP021", "Type":"Villa", "Name":"Villa_Peaceful"}
+
+Adoley_Resident = {"House_id":"Ad54","Type":"Apartment", "Name":"Adoley_Resident"}
+
+Paddocks = {"House_id":"Ad54","Type":"Bungalow", "Name":"Paddocks"}
+
+Mensah_Lodge = {"House_id":"ML896","Type":"Condo", "Name":"Mensah Lodge"}
+
+
+#............................new_data...................................
